708f731d32355f9599599288b2a2fee639da6fac
kawseribn <kawser.ibn.93@gmail.com>
2021-06-13 19:52:50 +0600

    global

--- global_main.py
+++ global_main.py
@@ -4,8 +4,6 @@ from hybrid import gen_key
 class global_mainclass:
   def __init__(self,img):
     self.generated_keys = gen_key(img)
+    print(self.generated_keys)
 
-
- 
-
-
+x = global_mainclass('/images/onez.jpg')
